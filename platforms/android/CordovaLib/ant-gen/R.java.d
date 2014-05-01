b2a5cb9818ff75636182c6ee7310d1b64fd58c56
Ross Wilson <me@rosswilson.co.uk>
2014-05-01 10:38:01 +0100

    Added JWPlayer JS files, going to wire it up now

---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/rosswilson/Desktop/vision-mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,10 @@
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes.dex : \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes/org/apache/cordova/splashscreen/SplashScreen.class \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes/uk/ac/lancs/vision/BuildConfig.class \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes/uk/ac/lancs/vision/R$attr.class \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes/uk/ac/lancs/vision/R$drawable.class \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes/uk/ac/lancs/vision/R$string.class \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes/uk/ac/lancs/vision/R$xml.class \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes/uk/ac/lancs/vision/R.class \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes/uk/ac/lancs/vision/Vision.class \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/dexedLibs/classes-82169902c97a9d58a44a46b3bc06d810.jar \
