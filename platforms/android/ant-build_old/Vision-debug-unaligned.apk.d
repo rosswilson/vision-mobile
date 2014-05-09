c9e02ef9a091eff77d1830e7cc14dd5b16aed347
Ross Wilson <me@rosswilson.co.uk>
2014-05-09 17:57:57 +0100

    Added icons for Android and triggered two full builds (clean and build) for iOS and Android.

---
+++ platforms/android/ant-build_old/Vision-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/Vision-debug-unaligned.apk : \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/Vision.ap_ \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/ant-build/classes.dex \
+/Users/rosswilson/Desktop/vision-mobile/platforms/android/CordovaLib/ant-build/classes.jar \
