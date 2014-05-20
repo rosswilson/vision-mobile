a7d8beb3a1879526439ed66f12ab6dbd0802c61e
Ross Wilson <me@rosswilson.co.uk>
2014-05-20 12:10:41 +0100

    Added percentage background progress bar to history programme items

--- platforms/ios/build/emulator/Vision.app/www/js/playback.js
+++ platforms/ios/build/emulator/Vision.app/www/js/playback.js
@@ -5,77 +5,66 @@ angular.module('vision')
 
   SetTitle("Playback");
 
-  // Programme ID to retrieve programme name, synopsis ect
   $scope.programme_id = $routeParams['programme_id'];
-
-  // Live channel id e.g. bbcone.stream
-  $scope.live_channel = $routeParams['live_channel'];
-
-  // Seconds into the VOD to start playhead
   $scope.start_at = $routeParams['start_at'] ? $routeParams['start_at'] : 0;
 
   $scope.last_segment_start = $scope.start_at;
   $scope.last_segment_end = $scope.start_at;
 
-  var success = function(data) {
-    $scope.programme = data;
+  var success = function(programme) {
+    $scope.programme = programme;
 
-    // If a live channel was passed, use it's stream URL, else use VOD url
-    if ($scope.live_channel) {
-      var video_url = ProgrammeService.get_live_url($scope.live_channel);
-    } else {
-      var video_url = ProgrammeService.get_vod_url($scope.programme);
-    }
+    if(programme.watch_live || programme.watch_catchup) {
+      PlayerStatsService.new_instance(programme.programme_id, programme.playback_url);
 
-    PlayerStatsService.new_instance($scope.programme_id, video_url);
+      // Set the video player poster image if not resuming (since we'll immediately start playing)
+      var player = document.getElementById('video-player');
+      player.setAttribute("poster", ProgrammeService.get_poster_url($scope.programme, 720, 405));
 
-    // Set the video player poster image if not resuming (since we'll immediately start playing)
-    var player = document.getElementById('video-player');
-    player.setAttribute("poster", ProgrammeService.get_poster_url($scope.programme, 720, 405));
-
-    player = new MediaElementPlayer('#video-player', {
-      type: ['video/mp4'],
-      success: function (mediaElement, domObject) {
-        mediaElement.setSrc(video_url);
-        mediaElement.load();
-
-        // If resuming, once player has media metadata, we can shift the playhead position
-        if($scope.start_at != 0) {
-          mediaElement.addEventListener("canplay", function() {
-            if($scope.start_at) {
-              mediaElement.setCurrentTime($scope.start_at);
-              $scope.start_at = null;
-            }
+      player = new MediaElementPlayer('#video-player', {
+        type: ['video/mp4'],
+        success: function (mediaElement, domObject) {
+          var resume_playback = function() {
+            mediaElement.setCurrentTime($scope.start_at);
             mediaElement.play();
-          });
-        }
+            mediaElement.removeEventListener("canplay", resume_playback);
+          };
 
-        var calculate_segment = function() {
-          var currentSeconds = Math.floor(mediaElement.currentTime);
-
-          // Proceed if not already updated segment and it's been 3 seconds since we last did
-          if(currentSeconds != $scope.last_segment_end && currentSeconds != 0 && currentSeconds % 3 == 0) {
-
-            // Check if skipped more than *5* seconds (not 3) incase timings are slightly inaccurate
-            if(currentSeconds < $scope.last_segment_end || currentSeconds > $scope.last_segment_end + 5) {
-              $scope.last_segment_start = currentSeconds;
-              // console.log("Logging skipped segment " + $scope.last_segment_start + ":" + currentSeconds);
-            }
-
-            PlayerStatsService.log_segment($scope.last_segment_start, $scope.last_segment_end);
-            $scope.last_segment_end = currentSeconds;
+          // If resuming, once player has media metadata, we can shift the playhead position
+          if($scope.start_at) {
+            mediaElement.addEventListener("canplay", resume_playback);
           }
+
+          var calculate_segment = function() {
+            var currentSeconds = Math.floor(mediaElement.currentTime);
+
+            // Proceed if not already updated segment and it's been 3 seconds since we last did
+            if(currentSeconds != $scope.last_segment_end && currentSeconds != 0 && currentSeconds % 3 == 0) {
+
+              // Check if skipped more than *5* seconds (not 3) incase timings are slightly inaccurate
+              if(currentSeconds < $scope.last_segment_end || currentSeconds > $scope.last_segment_end + 5) {
+                $scope.last_segment_start = currentSeconds;
+                // console.log("Logging skipped segment " + $scope.last_segment_start + ":" + currentSeconds);
+              }
+
+              PlayerStatsService.log_segment($scope.last_segment_start, $scope.last_segment_end);
+              $scope.last_segment_end = currentSeconds;
+            }
+          }
+
+          mediaElement.addEventListener('timeupdate', function() {
+            calculate_segment();
+          });
+
+          mediaElement.addEventListener('seeked', function() {
+            calculate_segment();
+          });
+
+          mediaElement.setSrc(programme.playback_url);
+          mediaElement.load();
         }
-
-        mediaElement.addEventListener('timeupdate', function() {
-          calculate_segment();
-        });
-
-        mediaElement.addEventListener('seeked', function() {
-          calculate_segment();
-        });
-      }
-    });
+      });
+    }
   };
 
   var error = function(error) {
@@ -86,46 +75,58 @@ angular.module('vision')
 
   $scope.watch_later = function() {
     var player = document.getElementById('video-player');
-    var current_time = Math.floor(player.currentTime);
-    var is_live = $scope.live_channel ? true : false;
-    WatchLaterService.store($scope.programme_id, current_time, is_live);
+    var current_time = 0;
+
+    if(player) {
+      var current_time = Math.floor(player.currentTime);
+    }
+
+    WatchLaterService.store($scope.programme_id, current_time, $scope.programme.watch_live);
   };
 })
 
 .service('ProgrammeService', function ($http, $q, QueryStringBuilder) {
-  var _url = 'http://vision.lancs.ac.uk:9110/modules/videometa/get_video_meta';
+  var _url = 'http://10.42.32.184/search2.php';
 
   return {
     get: function(programme_id) {
       var deferred = $q.defer();
       var params = {
-        api: '53e659a15aff4a402de2d51b98703fa1ade5b8c5',
-        programme_id: programme_id
+        q: programme_id,
+        qf: 'programme_id',
+        start: 0,
+        rows: 10,
+        url: '/future/select',
+        wt: 'json',
+        send_filters: false,
+        sort: 'score+desc'
       }
 
       var success = function (data, status, headers, config) {
-        if(data['num_res']) {
-          console.log(data);
-          var programme = data['data'][0];
-          programme['vod_url'] = 'http://148.88.32.70/' + programme_id + '.mp4';
+        var programme = data.response.docs[0];
+        if(programme) {
+
+          // Set the playback URL either to the VOD file or live stream address
+          if(programme.watch_catchup) {
+            programme['playback_url'] = 'http://148.88.32.70/' + programme.programme_id + '.mp4';
+          } else if(programme.watch_live) {
+            programme['playback_url'] = 'http://10.42.67.123:1935/live/mp4:' + programme.wowza_code + '/playlist.m3u8';
+          }
+
           deferred.resolve(programme);
+        } else {
+          deferred.reject("Unknown programme ID");
         }
       };
 
       var failure = function (data, status, headers, config) {
-        deferred.reject("Error getting currently airing JSON cache file");
+        deferred.reject("Error accessing SOLR engine to lookup programme ID");
       };
 
       var url = _url + '?' + QueryStringBuilder(params);
-      $http.get(url, { cache: true }).success(success).error(failure);
+      $http.get(url, { cache: false }).success(success).error(failure);
 
       return deferred.promise;
-    },
-    get_live_url: function(channel) {
-      return "http://10.42.67.123:1935/live/mp4:" + channel + "/playlist.m3u8";
-    },
-    get_vod_url: function(programme) {
-      return programme.vod_url;
     },
     get_poster_url: function(programme, width, height) {
       return "http://148.88.32.64/cache/" + width + "x" + height + "/programmes" + programme.image;
